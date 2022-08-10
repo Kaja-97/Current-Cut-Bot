fbdce6f5a351fd8e150280818f2b059f0dcb08c5
Kajanan <110709649+Kaja-97@users.noreply.github.com>
2022-08-10 13:20:12 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -12,10 +12,11 @@ import os
 import time
 import chromedriver_binary
 import telebot
-
-
-
-
+from telebot import types
+from dotenv import load_dotenv
+import datetime
+import selenium
+config = load_dotenv(".env")
 
 path="chromedriver.exe"
 options=webdriver.ChromeOptions()
@@ -24,15 +25,129 @@ options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-sh-usage")
 
-bot = telebot.TeleBot('1938707924:AAHknoxOQa0hTqDhOY5jD5pX-bpbY5QdHwo')
-
+API_KEY=os.getenv('API_KEY')
+bot = telebot.TeleBot(API_KEY)
+############################################### news paper###################################
+@bot.message_handler(commands=['Paper'])
+def greet(message):
+    try:
+        today=datetime.datetime.now().strftime('%Y-%m-%d')
+        chat_id=message.chat.id
+        pagelist=[]
+        paperlist=[]
+        for i in range(1,11):
+            ur='https://epaper.virakesari.lk/newspaper/Daily/main/{}#page-{}'.format(today,i)
+            pagelist.append(ur)
+        for i,ii in enumerate(pagelist,start=1):
+            options=webdriver.ChromeOptions()
+            options.headless = True
+            driver =webdriver.Chrome(executable_path=r"C:\Users\kajan\Desktop\Python\Web Scraping\chromedriver",options=options)
+            driver.get(url=ii)
+            soup=BeautifulSoup(driver.page_source,'html')
+            paperpg=soup.find('img',id='pageImage')['src']
+            bot.send_photo(chat_id, paperpg, protect_content=True ,disable_notification=True)
+    except:
+        bot.reply_to(message,'sorry , maintenance Break' )
 
 ########### telegram bot'''#########################################
+@bot.message_handler(commands=['A'])
+def greet(message):
+    bot.reply_to(message,A )
+    
+    
+@bot.message_handler(commands=['B'])
+def greet(message):
+    bot.reply_to(message,B )
+    
+    
+@bot.message_handler(commands=['C'])
+def greet(message):
+    bot.reply_to(message,C )
+   
+    
+@bot.message_handler(commands=['D'])
+def greet(message):
+    bot.reply_to(message,D )
+ 
+    
+@bot.message_handler(commands=['E'])
+def greet(message):
+    bot.reply_to(message,E )
+    
+    
+@bot.message_handler(commands=['F'])
+def greet(message):
+    bot.reply_to(message,F )
+   
+    
+@bot.message_handler(commands=['G'])
+def greet(message):
+    bot.reply_to(message,G )
+    
+    
+@bot.message_handler(commands=['H'])
+def greet(message):
+    bot.reply_to(message,H )
+  
+    
+@bot.message_handler(commands=['I'])
+def greet(message):
+    bot.reply_to(message,I )
+
+
+@bot.message_handler(commands=['J'])
+def greet(message):
+    bot.reply_to(message,J )
+
+@bot.message_handler(commands=['K'])
+def greet(message):
+    bot.reply_to(message,K )
+
+@bot.message_handler(commands=['L'])
+def greet(message):
+    bot.reply_to(message,L )
+
+@bot.message_handler(commands=['P'])
+def greet(message):
+    bot.reply_to(message,P )
+    print(tt)
+@bot.message_handler(commands=['Q'])
+def greet(message):
+    bot.reply_to(message,Q )
+
+@bot.message_handler(commands=['R'])
+def greet(message):
+    bot.reply_to(message,R )
+
+@bot.message_handler(commands=['S'])
+def greet(message):
+    bot.reply_to(message,S )
+    
+@bot.message_handler(commands=['T'])
+def greet(message):
+    bot.reply_to(message,T )
+
+@bot.message_handler(commands=['U'])
+def greet(message):
+    bot.reply_to(message,U )
+
+@bot.message_handler(commands=['V'])
+def greet(message):
+    bot.reply_to(message,V )
+
 @bot.message_handler(commands=['W'])
 def greet(message):
-    bot.reply_to(message, mmm)
+    bot.reply_to(message,W )
     
-
+@bot.message_handler(commands=['I love you'])
+def greet(message):
+    bot.reply_to(message,'Me too ❤️' )
+    
+@bot.message_handler(commands=['start'])
+def greet(message):
+#     user_first_name = str(message.chat.first_name) 
+    bot.reply_to(message, f"Hey!  \n Welcome 😍 \n,Please type your zone , (ex :- /A )  \n, Do you want to read today News paper Type /Paper ")
+    
 ########### telegram bot'''#########################################
 driver =webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=options)
 url='https://cebcare.ceb.lk/Incognito/DemandMgmtSchedule'
@@ -57,10 +172,74 @@ for a in gg:
     zone=a.find('span',class_='badge border border-light text-light fw-500').getText()
     Zone.append(zone)
 
-dic = {'Zone': Zone, 'Time': Time}
-table = pd.DataFrame(dic)
-tt = table[table['Zone'] == 'W']
-mmm = str(tt)
+
+
+dic={'Zone':Zone,'Time':Time}
+table=pd.DataFrame(dic)
+
+
+AA=table[table['Zone']=='A']
+A=str(AA)
+
+BB=table[table['Zone']=='B']
+B=str(BB)
+
+CC=table[table['Zone']=='C']
+C=str(CC)
+
+DD=table[table['Zone']=='D']
+D=str(DD)
+
+EE=table[table['Zone']=='E']
+E=str(EE)
+
+FF=table[table['Zone']=='F']
+F=str(FF)
+
+GG=table[table['Zone']=='G']
+G=str(GG)
+
+HH=table[table['Zone']=='H']
+H=str(HH)
+
+II=table[table['Zone']=='I']
+I=str(II)
+
+JJ=table[table['Zone']=='J']
+J=str(JJ)
+
+KK=table[table['Zone']=='K']
+K=str(KK)
+
+LL=table[table['Zone']=='LL']
+L=str(LL)
+
+PP=table[table['Zone']=='P']
+P=str(PP)
+
+QQ=table[table['Zone']=='Q']
+Q=str(QQ)
+
+RR=table[table['Zone']=='R']
+R=str(RR)
+
+SS=table[table['Zone']=='S']
+S=str(SS)
+
+TT=table[table['Zone']=='T']
+T=str(TT)
+
+UU=table[table['Zone']=='U']
+U=str(UU)
+
+VV=table[table['Zone']=='V']
+v=str(VV)
+
+WW=table[table['Zone']=='W']
+W=str(WW)
+
+
+
 
 bot.polling()
 
